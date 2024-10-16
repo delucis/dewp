70e71c985690a0516b97e064beb32932c074926a
Chris Swithinbank <swithinbank@gmail.com>
2024-10-16 22:17:01 +0200

    Add content-utils reference to docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
 			social: {
 				github: 'https://github.com/delucis/dewp',
 			},
-			sidebar: ['index', { label: 'Reference', items: ['reference/collections'] }],
+			sidebar: ['index', { label: 'Reference', autogenerate: { directory: 'reference' } }],
 		}),
 	],
 });
